99a37ca4eb12fd0bcbca4888c640cda47606624f
mrdomrivera-isd <mrdomrivera@gmail.com>
2025-08-24 19:16:22 -0400

    Delete job-tracker-render-plugplay directory

--- job-tracker-render-plugplay/web/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
-function App() {
-  return <div style={{padding:20}}>Job Tracker Web UI is running ✅</div>;
-}
-
-createRoot(document.getElementById('root')).render(<App />)
